2d93b293b8f65a5d5e96e16724f2f43e9cecc822
abdurrahman <abdurrahmanaikon@gmail.com>
2024-05-06 10:27:07 +0700

    Proses hasil pemanggilan AJAX di nomor (2) dan tampilkan data ke dalam bentuk tabel.

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
+function submitForm() {
+    // Mengambil nilai input dari form
+    var nim = document.getElementById("nim").value;
+    var bahasa = document.getElementById("bahasa").value;
+    var tujuan = document.getElementById("tujuan").value;
+    var perusahaan = document.getElementById("perusahaan").value;
+    var divisi = document.getElementById("divisi").value;
+    var jabatan = document.getElementById("jabatan").value;
+    var alamat = document.getElementById("alamat").value;
 
+    // Membuat objek data untuk dikirimkan ke server
+    var data = {
+        nim: nim,
+        bahasa: bahasa,
+        tujuan: tujuan,
+        perusahaan: perusahaan,
+        divisi: divisi,
+        jabatan: jabatan,
+        alamat: alamat
+    };
+
+    // Mengirim data ke server menggunakan AJAX
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Tampilkan pesan sukses atau error jika diperlukan
+            alert(this.responseText);
+            // Bersihkan form setelah pengiriman data berhasil
+            document.getElementById("input-form").reset();
+            // Refresh tampilan untuk menampilkan data terbaru
+            fetchData();
+        }
+    };
+    xmlhttp.open("POST", "insert_data.php", true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify(data));
+}
+
+function fetchData() {
     // Panggil AJAX untuk mengambil data JSON dari server
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
@@ -11,86 +49,44 @@
     };
     xmlhttp.open("GET", "get_data.php", true);
     xmlhttp.send();
-
-    // Fungsi untuk menampilkan data dalam tabel HTML
-    function displayData(data) {
-        var table = "<table border='1'>";
-        table += "<tr><th>Surat ID</th><th>Nama Mahasiswa</th><th>Jurusan</th><th>Bahasa Dokumen</th><th>Tujuan</th><th>Nama Perusahaan</th><th>Divisi</th><th>Jabatan</th><th>Alamat Perusahaan</th><th>Aksi</th></tr>";
-        for (var i = 0; i < data.length; i++) {
-            table += "<tr>";
-            table += "<td>" + data[i].Surat_ID + "</td>";
-            table += "<td>" + data[i].Mahasiswa_Nama + "</td>";
-            table += "<td>" + data[i].Mahasiswa_Jurusan + "</td>";
-            table += "<td>" + data[i].Surat_Bahasa + "</td>";
-            table += "<td>" + data[i].Surat_Tujuan + "</td>";
-            table += "<td>" + data[i].Perusahaan_Nama + "</td>";
-            table += "<td>" + data[i].Divisi_Nama + "</td>";
-            table += "<td>" + data[i].Jabatan_Nama + "</td>";
-            table += "<td>" + data[i].Perusahaan_Alamat + "</td>";
-            table += "<td><button onclick='deleteData(" + data[i].Surat_ID + ")'>Hapus</button></td>";
-            table += "</tr>";
-        }
-        table += "</table>";
-        document.getElementById("data-table").innerHTML = table;
-    }
-
-    // Fungsi untuk menghapus data dengan AJAX
-    function deleteData(id) {
-        if (confirm("Apakah Anda yakin ingin menghapus data?")) {
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    // Tampilkan pesan sukses atau error jika diperlukan
-                    alert(this.responseText);
-                    // Refresh tampilan setelah menghapus data
-                    window.location.reload();
-                }
-            };
-            xmlhttp.open("GET", "delete_data.php?id=" + id, true);
-            xmlhttp.send();
-        }
-    }
-
-    // Fungsi untuk menambahkan data baru
-function addData() {
-    var nama = document.getElementById("nama").value;
-    var jurusan = document.getElementById("jurusan").value;
-    var bahasa = document.getElementById("bahasa").value;
-    var tujuan = document.getElementById("tujuan").value;
-    var perusahaan = document.getElementById("perusahaan").value;
-    var divisi = document.getElementById("divisi").value;
-    var jabatan = document.getElementById("jabatan").value;
-    var alamat = document.getElementById("alamat").value;
-
-    // Validasi input
-    if (nama === "" || jurusan === "" || bahasa === "" || tujuan === "" || perusahaan === "" || divisi === "" || jabatan === "" || alamat === "") {
-        alert("Semua kolom input harus diisi!");
-        return;
-    }
-
-    // Data yang akan dikirim dalam format JSON
-    var data = {
-        nama: nama,
-        jurusan: jurusan,
-        bahasa: bahasa,
-        tujuan: tujuan,
-        perusahaan: perusahaan,
-        divisi: divisi,
-        jabatan: jabatan,
-        alamat: alamat
-    };
-
-    // Mengirim data baru ke server menggunakan AJAX
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            // Tampilkan pesan sukses atau error jika diperlukan
-            alert(this.responseText);
-            // Refresh tampilan setelah menambahkan data
-            window.location.reload();
-        }
-    };
-    xmlhttp.open("POST", "add_data.php", true);
-    xmlhttp.setRequestHeader("Content-Type", "application/json");
-    xmlhttp.send(JSON.stringify(data));
 }
+
+function displayData(data) {
+    var table = "<table border='1'>";
+    table += "<tr><th>NIM</th><th>Bahasa Dokumen</th><th>Tujuan</th><th>Nama Perusahaan</th><th>Divisi</th><th>Jabatan</th><th>Alamat Perusahaan</th></tr>";
+    for (var i = 0; i < data.length; i++) {
+        table += "<tr>";
+        table += "<td>" + data[i].NIM + "</td>";
+        table += "<td>" + data[i].BahasaDocument + "</td>";
+        table += "<td>" + data[i].Tujuan + "</td>";
+        table += "<td>" + data[i].NamaPerusahaan + "</td>";
+        table += "<td>" + data[i].NamaDivisi + "</td>";
+        table += "<td>" + data[i].NamaJabatan + "</td>";
+        table += "<td>" + data[i].AlamatPerusahaan + "</td>";
+        table += "</tr>";
+    }
+    table += "</table>";
+    document.getElementById("data-table").innerHTML = table;
+}
+
+// Memanggil fetchData saat halaman dimuat
+window.onload = fetchData;
+
+function displayData(data) {
+    var table = "<table border='1'>";
+    table += "<tr><th>NIM</th><th>Bahasa Dokumen</th><th>Tujuan</th><th>Nama Perusahaan</th><th>Divisi</th><th>Jabatan</th><th>Alamat Perusahaan</th></tr>";
+    for (var i = 0; i < data.length; i++) {
+        table += "<tr>";
+        table += "<td>" + data[i].NIM + "</td>";
+        table += "<td>" + data[i].BahasaDocument + "</td>";
+        table += "<td>" + data[i].Tujuan + "</td>";
+        table += "<td>" + data[i].NamaPerusahaan + "</td>";
+        table += "<td>" + data[i].NamaDivisi + "</td>";
+        table += "<td>" + data[i].NamaJabatan + "</td>";
+        table += "<td>" + data[i].AlamatPerusahaan + "</td>";
+        table += "</tr>";
+    }
+    table += "</table>";
+    document.getElementById("data-table").innerHTML = table;
+}
+
